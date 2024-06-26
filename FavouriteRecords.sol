dbf322d2a45ef20c69bd27bab80805eb042bcd1e
b1ometric <b1ometric@users.noreply.github.com>
2024-06-26 22:43:54 +0100

    Update FavouriteRecords.sol fix tabs

--- FavouriteRecords.sol
+++ FavouriteRecords.sol
@@ -15,10 +15,10 @@ A public mapping approvedRecords, which returns true if an album name has been a
 A mapping called userFavorites that indexes user addresses to a mapping of string record names which returns true or false, depending if the user has marked that album as a favorite
 */
 
-mapping (string => bool) public approvedRecords;
-mapping (address => string[]) public userFavorites;
-address[] private addressesOfUserFavourites;
-string[] private approvedRecordsArray;
+	mapping (string => bool) public approvedRecords;
+	mapping (address => string[]) public userFavorites;
+	address[] private addressesOfUserFavourites;
+	string[] private approvedRecordsArray;
 /*
 Loading Approved Albums
 Using the method of your choice, load approvedRecords with the following:
@@ -26,59 +26,59 @@ Using the method of your choice, load approvedRecords with the following:
 "Thriller", "Back in Black", "The Bodyguard", "The Dark Side of the Moon", "Their Greatest Hits (1971-1975)", "Hotel California", "Come On Over", "Rumours, "Saturday Night Fever"
 */
 //approvedRecords = ["Thriller", "Back in Black", "The Bodyguard", "The Dark Side of the Moon", "Their Greatest Hits (1971-1975)", "Hotel California", "Come On Over", "Rumours", "Saturday Night Fever"];
-constructor() {
-	 approvedRecords["Thriller"] = true;
-	 approvedRecords["Back in Black"] = true;
-	 approvedRecords["The Bodyguard"] = true;
-	 approvedRecords["The Dark Side of the Moon"] = true;
-	 approvedRecords["Their Greatest Hits (1971-1975)"] = true;
-	 approvedRecords["Hotel California"] = true;
-	 approvedRecords["Come On Over"] = true;
-	 approvedRecords["Rumours"] = true;
-	 approvedRecords["Saturday Night Fever"] = true;
+	constructor() {
+		 approvedRecords["Thriller"] = true;
+		 approvedRecords["Back in Black"] = true;
+		 approvedRecords["The Bodyguard"] = true;
+		 approvedRecords["The Dark Side of the Moon"] = true;
+		 approvedRecords["Their Greatest Hits (1971-1975)"] = true;
+		 approvedRecords["Hotel California"] = true;
+		 approvedRecords["Come On Over"] = true;
+		 approvedRecords["Rumours"] = true;
+		 approvedRecords["Saturday Night Fever"] = true;
 
-     approvedRecordsArray.push("Thriller");
-     approvedRecordsArray.push("Back in Black");
-     approvedRecordsArray.push("The Bodyguard");
-     approvedRecordsArray.push("The Dark Side of the Moon");
-     approvedRecordsArray.push("Their Greatest Hits (1971-1975)");
-     approvedRecordsArray.push("Hotel California");
-     approvedRecordsArray.push("Come On Over");
-     approvedRecordsArray.push("Rumours");
-     approvedRecordsArray.push("Saturday Night Fever");
-}
+	         approvedRecordsArray.push("Thriller");
+	         approvedRecordsArray.push("Back in Black");
+	         approvedRecordsArray.push("The Bodyguard");
+	         approvedRecordsArray.push("The Dark Side of the Moon");
+	         approvedRecordsArray.push("Their Greatest Hits (1971-1975)");
+	         approvedRecordsArray.push("Hotel California");
+	         approvedRecordsArray.push("Come On Over");
+	         approvedRecordsArray.push("Rumours");
+	         approvedRecordsArray.push("Saturday Night Fever");
+	}
 
 /*
 Get Approved Records
 Add a function called getApprovedRecords. This function should return a list of 
 all of the names currently indexed in approvedRecords.
 */
-function getApprovedRecords() external view returns (string[] memory){
-	uint cursor = 0;
-	uint resultsLength = _countFavouriteRecords();
-	
-	string[] memory tempApprovedRecords = new string[](resultsLength);
-	for (uint i=0; i < approvedRecordsArray.length; i++){
-		if (approvedRecords[approvedRecordsArray[i]] == true){
-			//_approvedRecords[cursor] = approvedRecordsArray[i];
-			tempApprovedRecords[cursor] = approvedRecordsArray[i];
-			cursor++;
+	function getApprovedRecords() external view returns (string[] memory){
+		uint cursor = 0;
+		uint resultsLength = _countFavouriteRecords();
+		
+		string[] memory tempApprovedRecords = new string[](resultsLength);
+		for (uint i=0; i < approvedRecordsArray.length; i++){
+			if (approvedRecords[approvedRecordsArray[i]] == true){
+				//_approvedRecords[cursor] = approvedRecordsArray[i];
+				tempApprovedRecords[cursor] = approvedRecordsArray[i];
+				cursor++;
+			}
 		}
+		//return _approvedRecords;
+		return tempApprovedRecords;
+	}	
+
+	function _countFavouriteRecords() internal view returns(uint) {
+	    uint result = 0;
+	
+	    for(uint i = 0; i < approvedRecordsArray.length; i++) {
+	        if(approvedRecords[approvedRecordsArray[i]] == true) {
+	            result++;
+	        }
+	    }
+	    return result;
 	}
-	//return _approvedRecords;
-	return tempApprovedRecords;
-}	
-
-function _countFavouriteRecords() internal view returns(uint) {
-    uint result = 0;
-
-    for(uint i = 0; i < approvedRecordsArray.length; i++) {
-        if(approvedRecords[approvedRecordsArray[i]] == true) {
-            result++;
-        }
-    }
-    return result;
-}
 
 /*
 Add Record to Favorites
@@ -103,16 +103,16 @@ Users' Lists
 Write a function called getUserFavorites that retrieves the list of favorites 
 for a provided address memory.
 */
-function getUserFavorites(address sender) external view returns(string[] memory) {
-	return userFavorites[sender];
-}
+	function getUserFavorites(address sender) external view returns(string[] memory) {
+		return userFavorites[sender];
+	}
 
 /*
 Reset My Favorites
 Add a function called resetUserFavorites that resets userFavorites for the sender.
 */
-function resetUserFavorites() external {
-	delete userFavorites[msg.sender];
-}
+	function resetUserFavorites() external {
+		delete userFavorites[msg.sender];
+	}
 
 }
